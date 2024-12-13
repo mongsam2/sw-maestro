8fa282604fd7ed978afca802f1c7eebcb198d509
mongsam2 <songjaehyun121476@gmail.com>
2024-12-13 11:32:45 +0900

    clear: 1504_특정한 최단 경로로

---
+++ gold/1504_특정한 최단 경로.py
@@ -0,0 +1,52 @@
+# https://www.acmicpc.net/problem/1504
+'''
+그래프 이론
+최단 경로
+데이크스트라
+'''
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+n, e = map(int, input().split())
+graph = {i: dict() for i in range(1, n+1)}
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+    graph[b][a] = c
+
+v1, v2 = map(int, input().split())
+
+def dijkstra(start, graph):
+    dist_list = [float("inf") for _ in range(len(graph.keys())+1)]
+    heap = [(0, start)]
+    dist_list[start] = 0
+
+    while heap:
+        current_dist, current_node = heapq.heappop(heap)
+
+        if current_dist > dist_list[current_node]:
+            continue
+
+        for next_node, next_dist in graph[current_node].items():
+            new_dist = current_dist + next_dist
+
+            if new_dist < dist_list[next_node]:
+                dist_list[next_node] = new_dist
+                heapq.heappush(heap, (new_dist, next_node))
+
+    return dist_list
+
+one_to = dijkstra(1, graph)
+n_to = dijkstra(n, graph)
+v1_to_v2 = dijkstra(v1, graph)[v2]
+
+path1 = one_to[v1] + n_to[v2]
+path2 = one_to[v2] + n_to[v1]
+answer = min(path1, path2) + v1_to_v2
+if answer == float("inf"):
+    print(-1)
+else:
+    print(answer)
